c11f81624f8c2d6956cafb09110afa47f00f50da
hakuya <931290928@qq.com>
2021-11-06 01:24:32 +0800

    Add tab and split keymap.

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -13,3 +13,14 @@ map('n', '<C-s>', '10l', noresilent)
 
 map('n', '<A-w>', ':w<CR>', noresilent)
 map('n', '<A-q>', ':q<CR>', noresilent)
+
+map('n', '<Space>tc', ':tabc<CR>', noresilent)
+map('n', '<Space>tn', ':tabn<CR>', noresilent)
+map('n', '<Space>tp', ':tabp<CR>', noresilent)
+
+map('n', '<Space>sh', ':sp<CR>' , noresilent)
+map('n', '<Space>sv', ':vsp<CR>', noresilent)
+map('n', '<A-w>'    , '<C-w>k'  , noresilent)
+map('n', '<A-r>'    , '<C-w>j'  , noresilent)
+map('n', '<A-a>'    , '<C-w>h'  , noresilent)
+map('n', '<A-s>'    , '<C-w>l'  , noresilent)
